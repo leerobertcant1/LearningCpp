ddd68f1b57a69f4e64339f4a5eb86ad2b849ce6d
Lee Cant <leerobertcant@gmail.com>
2020-07-11 15:45:29 +0100

    Started learning cpp

---
+++ LearningCpp/1_WhyCpp.cpp
@@ -0,0 +1,13 @@
+/*
+
+Direct control over hardware, good for performance.
+Provides direct control over hardware.
+
+Doesn't run a Virtual Machine - C Sharp/ Java are compiled at run time.
+Already in Machine code language so not needed.
+
+Virtual Machine code will be faster if bad code written.
+
+About good code/ fast code.
+
+*/
---
+++ LearningCpp/2_SetupWindows.cpp
@@ -0,0 +1,19 @@
+/*
+Visual Studio the best thing for C++.
+
+Download Community of the website.
+Desktop Dev with C++.
+Consider getting games extension if doing games.
+
+
+Dark mode: Tool/Options.
+
+*/
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello world ";
+	std::cin.get();
+}
